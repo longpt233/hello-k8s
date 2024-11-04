35d62b028afac5f5490bdba7833ee59f54f8f98a
Phan Thanh Long - BU.BI <longpt20@viettelpost.vn>
2024-11-04 18:03:34 +0700

    chya len cumj con loi day

---
+++ 6-kube-gpu/isssue.md
@@ -0,0 +1,112 @@
+1. 
+
+failed-to-run-kubelet-validate-service-connection-cri-v1-runtime-api-is-not-im
+
+FATA[0000] listing containers failed: rpc error: code = Unimplemented desc = unknown service runtime.v1alpha2.RuntimeService
+
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+sudo nano /etc/containerd/config.toml set SystemdCgroup = true
+sudo systemctl restart containerd
+
+
+root@hello:~# kubeadm config images pull --kubernetes-version=1.28.0 --v=5
+I1101 18:50:43.619872  235970 initconfiguration.go:117] detected and using CRI socket: unix:///var/run/containerd/containerd.sock
+I1101 18:50:43.620000  235970 interface.go:432] Looking for default routes with IPv4 addresses
+I1101 18:50:43.620008  235970 interface.go:437] Default route transits interface "enp3s0"
+I1101 18:50:43.620169  235970 interface.go:209] Interface enp3s0 is up
+I1101 18:50:43.620210  235970 interface.go:257] Interface "enp3s0" has 1 addresses :[10.208.164.167/22].
+I1101 18:50:43.620221  235970 interface.go:224] Checking addr  10.208.164.167/22.
+I1101 18:50:43.620230  235970 interface.go:231] IP found 10.208.164.167
+I1101 18:50:43.620244  235970 interface.go:263] Found valid IPv4 address 10.208.164.167 for interface "enp3s0".
+I1101 18:50:43.620252  235970 interface.go:443] Found active IP 10.208.164.167 
+I1101 18:50:43.620277  235970 kubelet.go:196] the value of KubeletConfiguration.cgroupDriver is empty; setting it to "systemd"
+[config/images] Pulled registry.k8s.io/kube-apiserver:v1.28.0
+[config/images] Pulled registry.k8s.io/kube-controller-manager:v1.28.0
+[config/images] Pulled registry.k8s.io/kube-scheduler:v1.28.0
+[config/images] Pulled registry.k8s.io/kube-proxy:v1.28.0
+[config/images] Pulled registry.k8s.io/pause:3.9
+[config/images] Pulled registry.k8s.io/etcd:3.5.15-0
+[config/images] Pulled registry.k8s.io/coredns/coredns:v1.10.1
+
+2. /proc/sys/net/bridge/bridge-nf-call-iptables 
+
+root@hello:~# kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=10.208.164.173
+I1104 16:52:39.935839 2876932 version.go:256] remote version is much newer: v1.31.2; falling back to: stable-1.28
+[init] Using Kubernetes version: v1.28.15
+[preflight] Running pre-flight checks
+	[WARNING HTTPProxy]: Connection to "https://10.208.164.173" uses proxy "http://10.208.164.185:9999/". If that is not intended, adjust your proxy settings
+	[WARNING HTTPProxyCIDR]: connection to "10.96.0.0/12" uses proxy "http://10.208.164.185:9999/". This may lead to malfunctional cluster setup. Make sure that Pod and Services IP ranges specified correctly as exceptions in proxy configuration
+	[WARNING HTTPProxyCIDR]: connection to "10.244.0.0/16" uses proxy "http://10.208.164.185:9999/". This may lead to malfunctional cluster setup. Make sure that Pod and Services IP ranges specified correctly as exceptions in proxy configuration
+error execution phase preflight: [preflight] Some fatal errors occurred:
+	[ERROR FileContent--proc-sys-net-bridge-bridge-nf-call-iptables]: /proc/sys/net/bridge/bridge-nf-call-iptables does not exist
+[preflight] If you know what you are doing, you can make a check non-fatal with `--ignore-preflight-errors=...`
+To see the stack trace of this error execute with --v=5 or higher
+root@hello:~# echo 1 > /proc/sys/net/bridge/bridge-nf-call-iptables
+-bash: /proc/sys/net/bridge/bridge-nf-call-iptables: No such file or directory
+root@hello:~# ls -la /proc/sys/net/
+core/             ipv4/             ipv6/             mptcp/            netfilter/        nf_conntrack_max  unix/             
+root@hello:~# modprobe -a overlay br_netfilter
+root@hello:~# echo 1 > /proc/sys/net/bridge/bridge-nf-call-iptables
+root@hello:~# 
+
+
+3. cni plugin not initialized
+
+Nov 04 17:10:38 hello kubelet[2889161]: E1104 17:10:38.583368 2889161 kubelet.go:2874] "Container runtime network not ready" networkReady="NetworkReady=false reason:NetworkPluginNotReady message:Network plugin returns error: cni plugin not initialized"
+
+
+curl -fLo cni-plugins-linux-amd64-v1.1.1.tgz \
+  https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
+sudo mkdir -p /opt/cni/bin
+sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+-> eo lien quan, set no proxy la dc
+
+export no_proxy=127.0.0.1,10.208.164.167 && kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=10.208.164.167
+
+4. 
+
+
+
+[preflight] FYI: You can look at this config file with 'kubectl -n kube-system get cm kubeadm-config -o yaml'
+error execution phase preflight: unable to fetch the kubeadm-config ConfigMap: this version of kubeadm only supports deploying clusters with the control plane version >= 1.30.0. Current version: v1.28.15
+
+-> remove cai cu tren woker di roi cai lai
+
+5.  ImagePullBackOff worker
+
+
+long@hello:~$ kubectl get pods --all-namespaces
+NAMESPACE     NAME                            READY   STATUS             RESTARTS   AGE
+kube-system   coredns-5dd5756b68-56zsk        0/1     Pending            0          21m
+kube-system   coredns-5dd5756b68-m2q88        0/1     Pending            0          21m
+kube-system   etcd-hello                      1/1     Running            5          21m
+kube-system   kube-apiserver-hello            1/1     Running            1          21m
+kube-system   kube-controller-manager-hello   1/1     Running            0          21m
+kube-system   kube-proxy-8hs49                0/1     ImagePullBackOff   0          8m27s
+kube-system   kube-proxy-p7hh9                1/1     Running            0          21m
+kube-system   kube-scheduler-hello            1/1     Running            0          21m
+
+long@hello:~$ cat /etc/systemd/system/docker.service.d/http-proxy.conf 
+[Service]
+Environment="HTTP_PROXY=http://10.60.117.103:8085/"
+Environment="HTTPS_PROXY=http://10.60.117.103:8085/"
+
+root@vupc:~# mkdir -p /etc/systemd/system/containerd.service.d
+root@vupc:~# nano /etc/systemd/system/containerd.service.d/http-proxy.conf
+root@vupc:~# sudo systemctl daemon-reload
+root@vupc:~# sudo systemctl restart containerd
+root@vupc:~# sudo systemctl show --property=Environment containerd
+Environment=HTTP_PROXY=http://10.60.117.103:8085/ HTTPS_PROXY=http://10.60.117.103:8085/
+
+
+long@hello:~$ kubectl delete pod --namespace=kube-system kube-proxy-8hs49
+pod "kube-proxy-8hs49" deleted
+
--- 6-kube-gpu/kubeadm.sh
+++ 6-kube-gpu/kubeadm.sh
@@ -13,42 +13,44 @@ sudo swapoff -a
 # Sau đó tắt swap mỗi khi khởi động trong /etc/fstab
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
-
+1. cai containerd
 # containerd
 curl -fLo containerd-1.6.14-linux-amd64.tar.gz \
   https://github.com/containerd/containerd/releases/download/v1.6.14/containerd-1.6.14-linux-amd64.tar.gz
 # Extract the binaries
 sudo tar Cxzvf /usr/local containerd-1.6.14-linux-amd64.tar.gz
-
 # Install containerd as a service
 sudo curl -fsSLo /etc/systemd/system/containerd.service \
   https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
-
 sudo systemctl daemon-reload
 sudo systemctl enable --now containerd
 
-# runc
+Cach 2
+
+sudo apt-get install containerd.io
+hinh nhu no da dc cai tu luc noa roi cay
+
+2. cai runc
 curl -fsSLo runc.amd64 \
   https://github.com/opencontainers/runc/releases/download/v1.1.3/runc.amd64
 sudo install -m 755 runc.amd64 /usr/local/sbin/runc
 
-# cni container network interface
+3. cni container network interface
 curl -fLo cni-plugins-linux-amd64-v1.1.1.tgz \
   https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
 sudo mkdir -p /opt/cni/bin
 sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
 
+
+4. mo mang
 # You need to enable overlay and br_netfilter kernel modules. Additionally, you need to allow iptables see bridged network traffic.
 
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-overlay
-br_netfilter
-EOF
+# cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+# overlay
+# br_netfilter
+# EOF
 
-sudo modprobe -a overlay br_netfilter
+# sudo modprobe -a overlay br_netfilter
 
 # Mặc định, kernel Linux không cho phép định tuyến các gói IPv4 giữa các giao diện mạng, ngăn cản việc truyền dữ liệu qua lại. 
 # sysctl params required by setup, params persist across reboots
@@ -61,14 +63,18 @@ EOF
 # Apply sysctl params without reboot
 sudo sysctl --system
 
+5. cai kubelet kubeadm kubectl
 
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
 # Add Kubernetes GPG key
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-
-# Add Kubernetes apt repository
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
 
-# Fetch package list
+# https://kubernetes.io/blog/2023/08/15/pkgs-k8s-io-introduction/
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 sudo apt-get update
 
 sudo apt-get install -y kubelet kubeadm kubectl
@@ -81,16 +87,32 @@ root@vupc:~# kubeadm config images pull
 failed to pull image "registry.k8s.io/kube-apiserver:v1.31.2": failed to pull image registry.k8s.io/kube-apiserver:v1.31.2: failed to pull and unpack image "registry.k8s.io/kube-apiserver:v1.31.2": failed to resolve reference "registry.k8s.io/kube-apiserver:v1.31.2": failed to do request: Head "https://registry.k8s.io/v2/kube-apiserver/manifests/v1.31.2": dial tcp 34.96.108.209:443: i/o timeout
 To see the stack trace of this error execute with --v=5 or higher
 
-https://github.com/kubernetes/kubeadm/issues/182
 
+##### mo proxy
+https://github.com/kubernetes/kubeadm/issues/182
 sudo systemctl set-environment HTTP_PROXY=10.60.117.103:8085
 sudo systemctl set-environment HTTPS_PROXY=10.60.117.103:8085
+khong can dau 
+root@hello:~# cat /etc/environment
+PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+http_proxy="http://10.208.164.185:9999/"
+https_proxy="http://10.208.164.185:9999/"
+ftp_proxy="http://10.208.164.185:9999/"
+
 sudo systemctl restart containerd.service
+
+##### end mo proxy
 sudo kubeadm config images pull
 
 
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=10.208.164.173
 
+export no_proxy=127.0.0.1,10.208.164.167 && kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=10.208.164.167
+
+export no_proxy=127.0.0.1,10.208.164.167 && kubeadm join 10.208.164.167:6443 --token av3kif.ell51auabe7k6zaa \
+	--discovery-token-ca-cert-hash sha256:32ba89fc377d8da013b34801ca7e2a0587eaedbb6d60543fad18cf1b1d811aeb
+
+--config=/etc/kubernetes/kubeadm-config.yaml
 
 control plane sẽ tự động phân bổ địa chỉ IP trong CIDR chỉ định cho các pod trên mọi node trong cụm cluster. Bạn sẽ cần phải chọn CIDR sao cho không trùng với bất kỳ dải mạng hiện có để tránh xung đột địa chỉ IP
 
@@ -116,6 +138,50 @@ error execution phase wait-control-plane: could not initialize a Kubernetes clus
 To see the stack trace of this error execute with --v=5 or higher
 
 
+systemctl status kubelet
+
+
 
 kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
+
+# ừa nó log ra console, méo phải stdout nên ko grep được
+systemctl status kubelet | grep E > /tmp/log-k8s.log && cat /tmp/log-k8s.log
+journalctl -xeu kubelet -n 2 | grep E > /tmp/log-k8s.log && cat /tmp/log-k8s.log
+
+
+
+
+long@hello:~$ kubectl get pods --all-namespaces
+NAMESPACE     NAME                            READY   STATUS    RESTARTS   AGE
+kube-system   coredns-5dd5756b68-56zsk        0/1     Pending   0          32m
+kube-system   coredns-5dd5756b68-m2q88        0/1     Pending   0          32m
+kube-system   etcd-hello                      1/1     Running   5          33m
+kube-system   kube-apiserver-hello            1/1     Running   1          33m
+kube-system   kube-controller-manager-hello   1/1     Running   0          33m
+kube-system   kube-proxy-p7hh9                1/1     Running   0          32m
+kube-system   kube-proxy-wzfks                1/1     Running   0          6s
+kube-system   kube-scheduler-hello            1/1     Running   0          33m
+long@hello:~$     kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+serviceaccount/weave-net created
+clusterrole.rbac.authorization.k8s.io/weave-net created
+clusterrolebinding.rbac.authorization.k8s.io/weave-net created
+role.rbac.authorization.k8s.io/weave-net created
+rolebinding.rbac.authorization.k8s.io/weave-net created
+daemonset.apps/weave-net created
+long@hello:~$ kubectl get pods --all-namespaces
+NAMESPACE     NAME                            READY   STATUS    RESTARTS      AGE
+kube-system   coredns-5dd5756b68-56zsk        1/1     Running   0             34m
+kube-system   coredns-5dd5756b68-m2q88        1/1     Running   0             34m
+kube-system   etcd-hello                      1/1     Running   5             34m
+kube-system   kube-apiserver-hello            1/1     Running   1             34m
+kube-system   kube-controller-manager-hello   1/1     Running   0             34m
+kube-system   kube-proxy-p7hh9                1/1     Running   0             34m
+kube-system   kube-proxy-wzfks                1/1     Running   2 (20s ago)   106s
+kube-system   kube-scheduler-hello            1/1     Running   0             34m
+kube-system   weave-net-5hz97                 2/2     Running   1 (35s ago)   47s
+kube-system   weave-net-p6x7c                 2/2     Running   1 (31s ago)   47s
+
+
+kubectl describe pod --all-namespaces
