8e92842652ec7deddc87c706ef091a9f82228400
Phan Thanh Long - BU.BI <longpt20@viettelpost.vn>
2024-12-13 15:36:33 +0700

    sync

---
+++ 9-ml-flow/trion-infer.sh
@@ -0,0 +1,11 @@
+mig: 
+https://github.com/NVIDIA/deepops/blob/master/docs/k8s-cluster/nvidia-mig.md
+
+
+https://www.kysuai.com/2023/01/26/Trien-khai-Triton-Inference-Server-tren-Google-Kubernetes-Engine/
+https://catalog.ngc.nvidia.com/orgs/nvidia/containers/tritonserver
+https://medium.com/@ashwin.mudhol/serving-and-managing-ml-models-with-mlflow-and-nvidia-triton-inference-server-16f657751da0
+https://github.com/YH-Wu/Triton-Inference-Server-on-Kubernetes
+https://github.com/triton-inference-server/server/blob/main/deploy/k8s-onprem/README.md
+
+https://medium.com/bumble-tech/gpu-powered-kubernetes-clusters-7fc6505125c
